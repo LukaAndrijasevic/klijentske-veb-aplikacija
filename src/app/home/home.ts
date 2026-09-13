578b3d1f96a2c69444245ef9b4c0bc7f0c61b61a
LukaAndrijasevic <luka.andrijasevic.21@singimail.rs>
2026-09-13 19:38:25 +0200

    Vezbe 04_2
    Improving the home page with dating format , importing images from API , usage of icons from angular material.

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -3,11 +3,14 @@ import { FlightModel }         from '../../models/flight.model';
 import { RouterLink }          from "@angular/router";
 import { MatButtonModule}      from '@angular/material/button';
 import { MatCardModule}        from '@angular/material/card';
+import {MatIconModule}         from '@angular/material/icon';
+import { Utils }               from '../utils';
 
 import axios from 'axios';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
-  imports: [RouterLink , MatButtonModule , MatCardModule],
+  imports: [RouterLink , MatButtonModule , MatCardModule , MatIconModule],
   selector:     'app-home',
   templateUrl:  './home.html',
   styleUrl:     './home.css',
@@ -15,8 +18,11 @@ import axios from 'axios';
 export class Home {
   flights = signal<FlightModel[]>([])
 
-  constructor() {
-    axios.get('https://flight.pequla.com/api/flight/list?type=departure')
-      .then(rsp => this.flights.set(rsp.data))
+  constructor(public utils: Utils) {
+    axios.get<FlightModel[]>('https://flight.pequla.com/api/flight/list?type=departure')
+      .then(rsp => {
+        const sorted = rsp.data.sort((f1 , f2) => new Date(f1.scheduledAt).getTime() - new Date(f2.scheduledAt).getTime())
+        this.flights.set(rsp.data)
+    })
   }
 }
