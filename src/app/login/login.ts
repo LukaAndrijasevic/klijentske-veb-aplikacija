20f6c2e35c3698abe66ef971f04c8c4a3c389c9a
LukaAndrijasevic <luka.andrijasevic.21@singimail.rs>
2026-09-14 18:53:09 +0200

    Vezbe 07_1 Error animation
    Error animation for incorrect login.

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -6,6 +6,7 @@ import { MatIconModule }    from '@angular/material/icon';
 import { MatInputModule }   from '@angular/material/input';
 import { AuthService }      from '../services/auth.service';
 import { Router }           from '@angular/router';
+import { Alerts }           from '../alert';
 
 @Component({
   imports:  [
@@ -36,6 +37,6 @@ export class Login {
       return
     }
 
-    alert('Invalid email or password!')
+    Alerts.error('Invalid email or password!')
   }
 }
