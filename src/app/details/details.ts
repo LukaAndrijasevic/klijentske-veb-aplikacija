27d4a3e915232b63048596acb338082ed61ec892
LukaAndrijasevic <luka.andrijasevic.21@singimail.rs>
2026-09-14 02:15:11 +0200

    Vezbe 05_1
    Description will be written in next commit.

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,11 +1,14 @@
 import { Component, signal }  from '@angular/core';
 import { ActivatedRoute }     from '@angular/router';
 import { FlightModel }        from '../models/flights.model';
-
+import { Utils }              from '../utils';
+import { MatCardModule }      from '@angular/material/card';
+import { DomSanitizer, SafeResourceUrl }    from '@angular/platform-browser';
 import axios from 'axios';
 
+
 @Component({
-  imports:        [],
+  imports:        [MatCardModule],
   selector:       'app-details',
   templateUrl:    './details.html',
   styleUrl:       './details.css',
@@ -14,11 +17,17 @@ import axios from 'axios';
 export class Details {
   flight = signal<FlightModel | null>(null)
 
-  constructor(route: ActivatedRoute) {
+  constructor(route: ActivatedRoute , public utils: Utils , private sanitizer: DomSanitizer) {
     route.params.subscribe(params=>{
       const id = params['id']
       axios.get(`https://flight.pequla.com/api/flight/${id}`)
       .then(rsp => this.flight.set(rsp.data))
     })
+  }
+
+  getMapUrl(destination: string): SafeResourceUrl {
+    const encoded = encodeURIComponent(destination);
+    const url = `https://www.google.com/maps?q=${encoded}&output=embed`;
+    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
   }
 }
